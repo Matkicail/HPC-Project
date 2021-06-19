cc2612d841ded4c61e848ab668ea1a656090f881
Matkicail <61425800+Matkicail@users.noreply.github.com>
2021-06-19 13:08:47 +0200

    Create fuzzyMeans.h

---
+++ fuzzyMeans.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "point.h"
+
+#ifndef fuzzyMEANS
+#define fuzzyMEANS
+
+// Fuzzy means clustering
+
+
+
+
+
+#endif
