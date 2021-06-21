397ed2748f4935a9161040aeb2e91a05435eae31
Jaredrhd <jaredrhd@gmail.com>
2021-06-21 13:39:32 +0200

    Started Cuda Implementation

--- KMeans/Headers/kMeans.cuh
+++ KMeans/Headers/kMeans.cuh
@@ -3,66 +3,87 @@
 #include <time.h>
 #include <math.h>
 #include <float.h>
-// Include the other two headers so we have access to their defines
+
 #include "kMeans.h"
 #include "../Helpers/point.h"
 
-// Idea here is to slosh betweeen the two with the following flow
-// send data
-// assign centres
-// calculate the centres new mean
-// receive data
-// cylce for ITTERATIONS
-// This would require two different dims for the blocks and threads respectively
-// One which matches to the calculateCentres which requires us to only look at the K centres
-// One which matches to the assignCluster which requires us to look at M points where M is vastly less than N
-
-// assume we give each cluster a thread
-// Note that the below would need the cluster in memory to conistently be the updated cluster
-// It would also require the data to be up to date given that we need it to be in the right cluster
-__global__ calculateCentres(Point *data, Point *cluster){
-    // First set the general stuff to 0.
-    // I.e stuff that all clusters have in common
-    int tid = threadIdx.x;
-    float sum[DIMENSIONS];
-    int count = 0;
-    for(int i = 0 ; i < DIMENSIONS ; i++){
-        sum[i] = 0;
-    }
-    // for every point
-    for(int i = 0 ; i < NUMPOINTS ; i++){
-        // if they are in my cluster
-        if(data[i].cluster == tid + 1){
-            // increase my total count
-            count++;
-            // increase the values I hold
-            for(int j = 0 ; j < DIMENSIONS ; j++){
-                sum[j] += data[i].values[j];
-            }
-        }
-    }
-    // Update our cluster
-    for(int i = 0 ; i < DIMENSIONS ; i++){
-        cluster->values[i] = sum[i] / count;
-    }
+__device__ float pointDistanceGPU(Point x, Point y)
+{
+     float dist = 0;
+     for (int i = 0; i < DIMENSIONS; i++)
+     {
+         float temp = (x.values[i] - y.values[i]);
+         dist += temp * temp;
+     }
+     return sqrtf(dist);
 }
 
-// Need to discuss this one with you.
-// Idea is either to span N data points based on maximum number of blocks/threads
-// This doesnt work really since we could do vastly more, so idea could be to assign M data points to each thread
-// That thread figures out the assignment for those M data points
-// Currently not coallesced and unsure if it will work, it relies on the serial implementation but instead with a num assigned and tid
-__global__ assignCluster(Point *data, Point *clusters, int numAssigned){
-    int tid = blockIdx.x*blockDim.x + threadIdx.x;
-    for(int i = 0 ; i < numAssigned; i++){
-        if(i + tid < NUMPOINTS){
-            float distance = FLT_MAX;
-            for(int j = 0 ; j < NUMCLUSTER ; j++){
-                float tempDist = pointDistance(kPoints[j],data[tid + i]);
-                if(tempDist < distance){
-                    distance = tempDist;
-                    data[tid + i].cluster = kPoints[j].cluster;
-                }
+// __global__ calculateCentres(Point *data, Point *cluster){
+//     // First set the general stuff to 0.
+//     // I.e stuff that all clusters have in common
+//     int tid = threadIdx.x;
+//     float sum[DIMENSIONS];
+//     int count = 0;
+//     for(int i = 0 ; i < DIMENSIONS ; i++){
+//         sum[i] = 0;
+//     }
+//     // for every point
+//     for(int i = 0 ; i < NUMPOINTS ; i++){
+//         // if they are in my cluster
+//         if(data[i].cluster == tid + 1){
+//             // increase my total count
+//             count++;
+//             // increase the values I hold
+//             for(int j = 0 ; j < DIMENSIONS ; j++){
+//                 sum[j] += data[i].values[j];
+//             }
+//         }
+//     }
+//     // Update our cluster
+//     for(int i = 0 ; i < DIMENSIONS ; i++){
+//         cluster->values[i] = sum[i] / count;
+//     }
+// }
+
+__global__ void assignCluster(Point *data, Point *kPoints, int dataSize, int clusterSize)
+{
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    int numAssigned = dataSize / blockDim.x;
+
+    //32 numbers
+    //16 threads
+
+    //0,    16
+    //1,    17
+    //2,    18
+    //3,    19
+    //4,    20
+    //5,    21
+    //6,    22
+    //7,    23
+    //8,    24
+    //9,    25
+    //10,   26
+    //11,   27
+    //12,   28
+    //13,   29
+    //14,   30
+    //15,   31
+
+
+    for(int i = 0 ; i < numAssigned; i++)
+    {
+        //Get the position to operate on
+        int pos = tid + i * dataSize;
+
+        float distance = FLT_MAX;
+        for(int j = 0 ; j < NUMCLUSTER ; j++)
+        {
+            float tempDist = pointDistanceGPU(kPoints[j], data[pos]);
+            if(tempDist < distance)
+            {
+                distance = tempDist;
+                data[pos].cluster = kPoints[j].cluster;
             }
         }
     }
